77ada177aa81b7fbfdcd735bac885a0e295cf429
Jaymar Westerlow <jaywesterlow71@gmail.com>
2021-02-17 17:09:22 +0100

    Les 2a & 2b toegevoegd

---
+++ SD-K-F2M3BO-Duurzaam-Huis-BO-main/les2A/myApi.php
@@ -0,0 +1,45 @@
+<?php
+        
+        
+        // Report all errors
+        // Dit is nu handig bij het testen
+        // In een productie omgeving doe je dit niet of anders
+        error_reporting(E_ALL);
+
+
+        
+        // my API reageert op GET en POST requests
+        print "<h3>hello from myAPI !!</h3>"; // say hello to client
+     
+        
+        $myTime = time(); // get the server time
+
+		if (!empty($_GET['data'])){  
+            $dataFromGet= $_GET['data'];
+            print "<h4>GET request ontvangen</h4>";
+            print "de data uit het GET request is: " . $dataFromGet;
+            switch($dataFromGet){
+                case "server":
+                        print "<pre>my server variables are:  " ;
+                        print_r($_SERVER);  // show server data
+                        print "</pre>"; 
+                    break;
+                case "tijd":
+                        print "<br>server tijd: " . date("h:i:s",$myTime);
+                    break;
+
+                 // hier moet jij zelf code schrijven
+                    
+                case "easter egg":
+                    print " <br> Hallo, ik ben Jaymar";
+                    break;
+                case "jaymar":
+                    // jouw code hier
+                    print "&#128534;";
+                    break;
+                  // einde eigen code  
+                default:
+                    print "<h3>please read the manual for valid requests &#128540;</h3>";
+            }
+        }     
+?>
